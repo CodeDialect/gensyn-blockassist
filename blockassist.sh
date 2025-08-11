6278358ca6583bb03a7f35445dfe09045ea8b9be
CodeDialect <94617063+CodeDialect@users.noreply.github.com>
2025-08-11 18:18:05 +0530

    Update blockassist.sh

--- blockassist.sh
+++ blockassist.sh
@@ -58,7 +58,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt install -y \
     build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
     libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev \
-    libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev zip
 fi
 
 # ==== 5. Install pyenv ====
